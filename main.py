45d903739142686353217fe67e3f093be71295f0
doddy bubble <george.ziesler@gmail.com>
2025-07-29 13:18:32 +0100

    main.py debugged, quote marks removed from first function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def get_book_text("path_to_file"):
-    with open("path_to_file") as f:
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents 
 
